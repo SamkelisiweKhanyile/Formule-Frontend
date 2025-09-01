d1a409e738520bd5aaab2bffa8b0b96e0f381db8
Tsholofelo Mabidikane <165083310+Tsholo0707@users.noreply.github.com>
2025-09-02 01:31:35 +0200

    updated index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,54 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-import ProductList from '@/views/business/ProductList.vue'
-import CartPage from '@/views/business/CartPage.vue'
-import Checkout from '@/views/business/Checkout.vue'
-
-
-// Admin
-import AdminDashboard from '@/views/users/admin/AdminDashboard.vue'
-
-// Customer
-import customerProfile from '@/views/users/customer/CustomerProfile.vue'
-
-// Generic
-import AuthPage from '@/views/users/AuthPage.vue'
-
-
+import { createRouter, createWebHistory } from 'vue-router';
+import ProductList from '@/views/business/ProductList.vue';
+import Checkout from '@/views/business/Checkout.vue';
+import Login from '@/views/users/Login.vue';
 
 const routes = [
-  { path: '/', name: 'Home', component: ProductList },
-  { path: '/products', name: 'Products', component: ProductList },
-  { path: '/cart', name: 'Cart', component: CartPage },
-  { path: '/checkout', name: 'Checkout', component: Checkout },
-  { path: '/auth', name: 'Auth', component: AuthPage },
-  { path: '/customer/profile', name:'cProfile', component:customerProfile},
-  { path: '/admin', name: 'AdminDashboard', component: AdminDashboard },
-
-
-  { path: '/:pathMatch(.*)*', redirect: '/' }
-]
-
+  { path: '/', component: ProductList },
+  { path: '/checkout', component: Checkout },
+  { path: '/login', component: Login }
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-})
+});
 
-
-router.beforeEach((to, from, next) => {
-  const user = JSON.parse(localStorage.getItem('user'))
-
-
-  if (to.name === 'AdminDashboard') {
-    if (!user || user.role !== 'admin') {
-      return next({ name: 'Auth' })
-    }
-  }
-
-
-
-  next() 
-})
-
-export default router
+export default router;
